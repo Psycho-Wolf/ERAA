26bc6facab331b81afac43c316d8228f5cddfe31
Psycho-Wolf <antony_pavel@hotmail.com>
2022-01-27 13:06:03 -0700

    Ingredient and profit list complete

--- Official/MAIN SCREEN v2.py
+++ Official/MAIN SCREEN v2.py
@@ -44,7 +44,7 @@ def ORDER():
 
 def CheckOut():
     global profit
-    print profit
+    print profit   #FOR TESTING DELETE IN FINAL VERSION
     profit = 0
     var.set(5)
     list = text_box.get('1.0','end')
@@ -63,7 +63,33 @@ def CheckOut():
     order = textfile.read()
     global labelOrder
     labelOrder.destroy()
+    return
     
+def Stonks(amnts):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    stonks = open(timestr + '.txt', 'w')
+    stonks.write("Vodka: " + str(amnts[0]) + '\n')
+    stonks.write("Gin: " + str(amnts[1]) + '\n')
+    stonks.write("Rum: " + str(amnts[2]) + '\n')
+    stonks.write("Tequila: " + str(amnts[3]) + '\n')
+    stonks.write("Cranberry Juice: " + str(amnts[4]) + '\n')
+    stonks.write("Lime Juice: " + str(amnts[5]) + '\n')
+    stonks.write("Lemon Juice: " + str(amnts[6]) + '\n')
+    stonks.write("Coca Cola: " + str(amnts[7]) + '\n')
+    stonks.write("Campari: " + str(amnts[8]) + '\n')
+    stonks.write("Cointreau: " + str(amnts[9]) + '\n')
+    stonks.write("Grenadine: " + str(amnts[10]) + '\n')
+    stonks.write("Simple Syrup: " + str(amnts[11]) + '\n')
+    stonks.write("Tonic Water: " + str(amnts[12]) + '\n')
+    stonks.write("Dry Vermouth: " + str(amnts[13]) + '\n')
+    stonks.write("Kahlua: " + str(amnts[14]) + '\n')
+    stonks.write("Ice: " + str(amnts[15]) + '\n')
+    stonks.write("\nGross Profit: $" + str(profit))
+    stonks.close()
+    return
+
+
+
 def cosmo(amnts):
     global profit
     profit += price
