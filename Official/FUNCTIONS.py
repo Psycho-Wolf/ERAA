c69638b59e6d3251fc5ca8d42ef510dfae38617e
Psycho-Wolf <antony_pavel@hotmail.com>
2022-03-07 14:18:47 -0700

    Matching code from FXYx Console
    Copy pasted code from computer to github

--- Official/FUNCTIONS.py
+++ Official/FUNCTIONS.py
@@ -1,61 +1,136 @@
 # -*- coding: cp1252 -*-
 #imports
-#import serial #Dr. Isenbergs' python code to demo the FXYx robot.
+import serial #Dr. Isenbergs' python code to demo the FXYx robot.
 import time
-#import math
+import math
 from Tkinter import *
 import os
 from PIL import ImageTk, Image
 import tkMessageBox
 #from FXYx import *
 
+
+ser = serial.Serial('/dev/ttyS0')
+ser.baudrate=9600
+ser.parity=serial.PARITY_ODD
+ser.stopbits=serial.STOPBITS_ONE
+ser.bytesize=serial.EIGHTBITS
+ser.xonxoff=1
+ser.reset_input_buffer()
+ser.reset_output_buffer()
+
+def servosOn():  #turn on all servos
+	ser.write("@SRVO "+str(1)+chr(13)+chr(10))
+	rec=ser.read(4)
+	print "Servo On Reponse=",rec	#can only return ok
+	return
+
+def point(x,y,speed):
+    ser.write("@MOVD "+str(x)+","+str(y)+","+str(speed)+chr(13)+chr(10))
+    rec=ser.read(4)
+    print "Point Response=",rec
+    if rec[0]=="N":
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+    return;
+
+def HomePos():
+    ser.write("@MOVD "+str(0)+","+str(40)+","+str(50)+chr(13)+chr(10))
+    rec=ser.read(4)
+    print "Point Response=",rec
+    if rec[0]=="N":
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+    return;
+
+def origin():
+    ser.write("@ORG "+chr(13)+chr(10))
+    rec=ser.read(4)
+    print "Origin Response=",rec
+    if rec[0]=="N":
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+    else:
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+        i=0
+        rec=ser.read(1)
+        while rec[i]<>chr(10):
+            rec=rec+ser.read(1)
+            i=i+1
+        print rec
+    return;
+
+
 def cosmoBuild():
-    time.sleep(5)
-    point(540,300,80)
-    time.sleep(5)
-    origin()
+    time.sleep(1)
+    point(100,100,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def negroniBuild():
-#    time.sleep(5)
-#    point(100,100,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(100,200,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def russianBuild():
-#    time.sleep(5)
-#    point(540,300,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(200,100,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def liitBuild():
-#    time.sleep(5)
-#    point(100,100,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(200,200,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def cubaBuild():
-#    time.sleep(5)
-#    point(540,300,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(200,300,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def johnBuild():
-#    time.sleep(5)
-#    point(100,100,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(300,200,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def dryBuild():
-#    time.sleep(5)
-#    point(100,100,80)
-#    time.sleep(5)
-#    origin()
+    time.sleep(1)
+    point(300,300,80)
+    time.sleep(1)
+    HomePos()
     return
 
 def settings():
@@ -73,11 +148,11 @@ def settings():
         for line in lines:
             file.write(" ")
         file.close()
-        
+
         # ser.close()
-        
+
     # tally up overall profits of day
-        
+
         # volumeLabel = label("vodka: ", excelfile row1 col2).pack()
     # repeat for each ingredient
         return
